00a595dc1a41b06177aaef6c1f263170d39e73af
natsukosunaga <sunagamail222@gmail.com>
2022-06-01 00:24:47 +0900

    fix: validation追記

--- validate-config.js
+++ validate-config.js
@@ -1,15 +1,40 @@
-$("form_id").validate({
+$('form').validate({
     rules: {
-        text_name: {
+        text_name_1: {
             required: true,
+            minlength: 3,
         },
+
     },
     messages: {
-        text_name: {
-            required: "入力必須項目",
+        text_name_1: {
+            required: "入力必須項目です。",
+            minlength: "3文字以上入力してください。",
         },
     },
+    errorPlacement: function (err, elem) {
+        err.appendTo($('.error_1')); //p要素を指定
+    }
 });
+
+$('input[name="text_name_1"]').on('focusout', function () {
+    if ($('input[name="text_name_1"]').valid()) {
+        $('.OK').show();
+        $('#text_name_2').focus();
+    } else {
+        $('.OK').hide()
+    }
+});
+//enter
+// $('input[name="text_name"]').on('keyup', function () {
+//     if (!$('form').valid()) {
+//         alert('入力エラーがあります');
+//     } else {
+//         alert('OK');
+//     }
+// });
+
+
 
 // $("バリデーション対象フォームid").validate({
 //     rules: {
